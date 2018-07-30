f46448fab13dfc9e23d353df28ba62c33fd82319
zm17346 <zm17346@josai.ac.jp>
2018-07-29 21:45:23 -0400

    hello

---
+++ ex06-7.js
@@ -0,0 +1,7 @@
+var who ='world';
+function sayhello() {
+alert('hello,' + who + '!');
+}
+function someone(x) {
+who = x;
+}
---
+++ myjs.js
@@ -0,0 +1,9 @@
+//alert('Hello, Hinano!');
+//alert(1+1);
+alert(Math.sqrt(1+1));
+2+3
+'2'+'3'
+Number('2')+Number('3')
+'2'+3
+Number('2')+3
+
