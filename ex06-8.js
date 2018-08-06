dd4be5f515723b8cf3d87d64a57bd808e725a16e
zm17346 <zm17346@josai.ac.jp>
2018-08-06 03:01:52 -0400

    hello

--- ex06-8.js
+++ ex06-8.js
@@ -1,9 +1,9 @@
 function sayhello68() {
 alert('Hello,' + document.getElementByld('who').innerHTML + '!');
 }
-function taro() {
- document.getElementByld('who').innerHTML + '太郎';
+function 太郎() {
+ document.getElementByld('who').innerHTML + 'taro';
 }
-function hanako() {
- document.getElementByld('who').innerHTML + '花子';
+function 花子() {
+ document.getElementByld('who').innerHTML + 'hanako';
 }
--- ex6-5.js
+++ ex6-5.js
@@ -2,9 +2,6 @@ var who='world';
 function sayhello() {
    alert('Hello,'+ who + '!');
 }
-function taro(){
-  who='太郎';
-}
-function hanako(){
-  who='花子';
+function　太郎(){
+  who='taro';
 }
